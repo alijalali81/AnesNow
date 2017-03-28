2d3cc6d53c21df17afd30ab7761866171f0c8301
Ali Jalali <ali.jalali@outlook.com>
2017-03-28 18:15:55 -0400

    Data read

---
+++ anes_now_data_read.m
@@ -0,0 +1,45 @@
+
+fid=fopen('1.txt');
+formatSpec = '%s %s %d %{yyy-mm-dd hh:mm:ss}D %d %s %d %s %s %d %s %d %s %s %s %s %s';
+D = textscan(fid,formatSpec,'Delimiter','|');
+D(:,[1:3,5,10:17])=[];
+fclose(fid);
+
+t=datetime(D{1});
+h=t.Hour;
+m=t.Minute;
+s=t.Second;
+new_t=[h m s];
+f=[3600;60;1];
+tt=new_t*f;
+clear f new_t h m s t
+
+vital_label=D{2};
+vitals=unique(vital_label);
+vital_value=cell2mat(D(3));
+source_name=D{5};
+%source=unique(source_name);
+
+for i=1:length(vitals)
+    inx=strcmp(vital_label,vitals{i});
+    vname=vitals{i};
+    time=tt(inx==1);
+    source=source_name(inx==1);
+    value=vital_value(inx==1);
+    dd=table(time,source,value);
+    patient{i}=dd;
+end
+clear i tt
+
+for i=1:length(vitals)
+    n_vitals{i,1}=strrep(vitals{i},'-','_');
+    n_vitals{i,1}=strrep(n_vitals{i},' ','_');
+end
+clear i
+   
+m_patient=cell2struct(patient,n_vitals,2);
+
+clear n_vitals inx vname time source
+clear value dd patient source_name vital_value
+clear vital_label formatSpec
+clear D fid ans vitals
